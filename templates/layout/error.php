acdfad04d9cca49056569bb0c2c2e659d7db8545
Mateusz Mleczko <mateusz.mleczko2005@gmail.com>
2026-06-03 00:08:04 +0200

    feat(TAS-3): add bootstrap 5, bootstrap icons, color variables and vite

--- templates/layout/error.php
+++ templates/layout/error.php
@@ -1,39 +1,29 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
  * @var \App\View\AppView $this
  */
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="pl">
 <head>
     <?= $this->Html->charset() ?>
-    <title>
-        <?= $this->fetch('title') ?>
-    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $this->fetch('title') ?> | TaskHub</title>
     <?= $this->Html->meta('icon') ?>
-
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
-
-    <?= $this->fetch('meta') ?>
-    <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="error-container">
+<main class="py-5">
+    <div class="container">
         <?= $this->Flash->render() ?>
         <?= $this->fetch('content') ?>
-        <?= $this->Html->link(__('Back'), 'javascript:history.back()') ?>
+        <a href="javascript:history.back()" class="btn btn-outline-primary mt-3">
+            <i class="bi bi-arrow-left me-1"></i>Wróć
+        </a>
     </div>
+</main>
+<script src="/js/bootstrap.js"></script>
 </body>
 </html>
