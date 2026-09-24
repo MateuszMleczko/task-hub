b5b4bea9318a315e4b9b9f79e3b2e9a8068034c6
Mateusz Mleczko <mateusz.mleczko2005@gmail.com>
2026-09-24 22:02:14 +0200

    feat(TAS-45): add fonts to the project locally instead of requests to google every time

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,8 +14,6 @@
     <?= $this->Html->css('mainStyle') ?>
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg">
